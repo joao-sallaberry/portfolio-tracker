7262965280077219cf488b56cd611de1561fbe64
Joao Sallaberry <joaovictorhs@gmail.com>
2026-05-10 23:54:09 -0300

    redirect to auth page when email is confirmed

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -43,6 +43,9 @@ export default function Auth() {
             const { error } = await supabase.auth.signUp({
                 email,
                 password,
+                options: {
+                    emailRedirectTo: `${window.location.origin}/auth`,
+                },
             });
 
             if (error) throw error;
